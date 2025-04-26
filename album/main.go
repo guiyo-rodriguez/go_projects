dddb135baca37eba56688834d2fa536d28219e29
guiyo-rodriguez <guiller_2912@yahoo.com.ar>
2025-04-26 17:35:37 -0300

    Initial commit..

---
+++ album/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Album struct {
+    ID     int64
+    Title  string
+    Artist string
+    Price  float32
+}
+
+var db *sql.DB
+
+// albumsByArtist queries for albums that have the specified artist name.
+func albumsByArtist(name string) ([]Album, error) {
+    // An albums slice to hold data from returned rows.
+    var albums []Album
+
+    rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+    if err != nil {
+        return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+    }
+    defer rows.Close()
+    // Loop through rows, using Scan to assign column data to struct fields.
+    for rows.Next() {
+        var alb Album
+        if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+            return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+        }
+        albums = append(albums, alb)
+    }
+    if err := rows.Err(); err != nil {
+        return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+    }
+    return albums, nil
+}
+
+func main() {
+	// Reemplazá estos datos con los de tu servidor MySQL
+	dsn := "guille:6z6b6ch3@tcp(192.168.0.5:3306)/recordings"
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal("Error al abrir la conexión:", err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Error al conectar a la base de datos:", err)
+	}
+
+	fmt.Println("✅ ¡Conexión exitosa a MySQL!")
+
+albums, err := albumsByArtist("John Coltrane")
+if err != nil {
+    log.Fatal(err)
+}
+fmt.Printf("Albums found: %v\n", albums)
+}
